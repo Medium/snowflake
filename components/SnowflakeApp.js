b43d919ee408760bfcc15a5f3fadf4f8bfb526cf
Cliff Rodgers <kliph@users.noreply.github.com>
2017-10-30 14:31:38 -0400

    Use PromptWorks title and link instead of Medium

--- components/SnowflakeApp.js
+++ components/SnowflakeApp.js
@@ -4,7 +4,6 @@ import TrackSelector from '../components/TrackSelector'
 import NightingaleChart from '../components/NightingaleChart'
 import KeyboardListener from '../components/KeyboardListener'
 import Track from '../components/Track'
-import Wordmark from '../components/Wordmark'
 import LevelThermometer from '../components/LevelThermometer'
 import { eligibleTitles, trackIds, milestones, milestoneToPoints } from '../constants'
 import PointSummaries from '../components/PointSummaries'
@@ -155,9 +154,9 @@ class SnowflakeApp extends React.Component<Props, SnowflakeAppState> {
             text-decoration: none;
           }
         `}</style>
-        <div style={{margin: '19px auto 0', width: 142}}>
-          <a href="https://medium.com/" target="_blank">
-            <Wordmark />
+        <div>
+          <a href="https://promptworks.com/" target="_blank">
+            <h1>PromptWorks</h1>
           </a>
         </div>
         <div style={{display: 'flex'}}>
