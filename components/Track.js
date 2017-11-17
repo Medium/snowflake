1cecbced4cf8f3d8194c2cf3d52e10d7adbae904
Tyler Suderman <tcsuder@gmail.com>
2017-11-17 14:20:36 -0800

    add empty Ticket component - not integrated

---
+++ components/Track.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react'
+import { trackIds, tracks, categoryColorScale } from '../constants'
+import type { MilestoneMap, TrackId } from '../constants'
+
+type Props = {
+  milestoneByTrack: MilestoneMap,
+  focusedTrackId: TrackId,
+  setFocusedTrackIdFn: (TrackId) => void
+}
+
+function Track(props) {
+  
+}
