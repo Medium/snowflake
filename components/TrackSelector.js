eac71ce54a38893b445aaa2989498cba7671fcef
Tyler Suderman <tcsuder@gmail.com>
2017-11-17 13:44:53 -0800

    create multi-demensional display array to render table of track components

--- components/TrackSelector.js
+++ components/TrackSelector.js
@@ -11,7 +11,33 @@ type Props = {
 }
 
 function TrackSelector(props) {
-  let row = [];
+
+  function buildDisplayArray(trackIds) {
+    let displayRow = [];
+    let displayArray = [];
+
+    trackIds.map(trackId => {
+      let track = (<td key={trackId} className="track-selector-value"
+      style={{border: '4px solid ' + (trackId == props.focusedTrackId ? '#000': categoryColorScale(tracks[trackId].category)), background: categoryColorScale(tracks[trackId].category)}}
+      onClick={() => props.setFocusedTrackIdFn(trackId)}>
+      {props.milestoneByTrack[trackId]}
+      </td>)
+      displayRow.push(track)
+      let rowLength = displayRow.length
+
+      if (rowLength === 2) {
+        displayArray.push(displayRow)
+        displayRow = []
+      } else if (rowLength > 2) {
+        row.splice(0, 2)
+      }
+    })
+    console.log(displayArray);
+    return displayArray
+  }
+
+  buildDisplayArray(trackIds)
+
   return (
     <table>
       <style jsx>{`
@@ -39,10 +65,7 @@ function TrackSelector(props) {
       <tbody>
       <tr>
       {
-        trackIds.map(trackId => {
-          let rowLength = row.length
-          console.log(rowLength)
-        })
+
       }
       </tr>
         <tr>
@@ -69,70 +92,5 @@ function TrackSelector(props) {
     </table>
   )
 }
-
-// class TrackSelector extends React.Component<Props> {
-//   constructor(props: Props) {
-//     super(props)
-//   }
-//
-//   render() {
-//     return (
-//       <table>
-//         <style jsx>{`
-//           table {
-//             width: 100%;
-//             border-spacing: 10px;
-//             margin-left: -10px;
-//             margin-top: -10px;
-//           }
-//           .track-selector-value {
-//             line-height: 50px;
-//             width: 50px;
-//             text-align: center;
-//             background: #eee;
-//             font-weight: bold;
-//             font-size: 24px;
-//             border-radius: 3px;
-//             cursor: pointer;
-//           }
-//           .track-selector-label {
-//             text-align: center;
-//             font-size: 9px;
-//           }
-//         `}</style>
-//         <tbody>
-          // {
-          //   trackIds.map(trackId => {
-          //     let rowLength = row.length
-          //     console.log(rowLength)
-          //   })
-          // }
-          // <tr>
-          // </tr>
-//           <tr>
-//             {trackIds.map(trackId => (
-//               <td key={trackId} className="track-selector-label" onClick={() => this.props.setFocusedTrackIdFn(trackId)}>
-//                 {tracks[trackId].displayName}
-//               </td>
-//             ))}
-//           </tr>
-//           <tr>
-//             {trackIds.map(trackId =>
-//               {
-//                 return (
-//                   <td key={trackId} className="track-selector-value"
-//                   style={{border: '4px solid ' + (trackId == this.props.focusedTrackId ? '#000': categoryColorScale(tracks[trackId].category)), background: categoryColorScale(tracks[trackId].category)}}
-//                   onClick={() => this.props.setFocusedTrackIdFn(trackId)}>
-//                   {this.props.milestoneByTrack[trackId]}
-//                   </td>
-//                 )
-//               }
-//             )}
-//           </tr>
-//         </tbody>
-//       </table>
-//     )
-//   }
-// }
 
 export default TrackSelector
