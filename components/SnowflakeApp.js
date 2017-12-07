b4797e3838065203df1666eb9d719a99039eabf4
tcsuder@gmail.com <tcsuder@gmail.com>
2017-12-06 19:52:39 -0800

    Modify default state milestone values

--- components/SnowflakeApp.js
+++ components/SnowflakeApp.js
@@ -47,50 +47,30 @@ const emptyState = (): SnowflakeAppState => {
     name: '',
     title: '',
     milestoneByTrack: {
-      'MOBILE': 0,
-      'WEB_CLIENT': 0,
-      'FOUNDATIONS': 0,
-      'SERVERS': 0,
-      'PROJECT_MANAGEMENT': 0,
-      'COMMUNICATION': 0,
-      'CRAFT': 0,
-      'INITIATIVE': 0,
-      'CAREER_DEVELOPMENT': 0,
-      'ORG_DESIGN': 0,
-      'WELLBEING': 0,
-      'ACCOMPLISHMENT': 0,
-      'MENTORSHIP': 0,
-      'EVANGELISM': 0,
-      'RECRUITING': 0,
-      'COMMUNITY': 0
+      'SELF': 0,
+      'TEAM': 0,
+      'PEERS': 0,
+      'SUPERIORS': 0,
+      'BUSINESS': 0,
+      'HEALTH': 0
     },
-    focusedTrackId: 'MOBILE'
+    focusedTrackId: 'SELF'
   }
 }
 
 const defaultState = (): SnowflakeAppState => {
   return {
-    name: 'Cersei Lannister',
+    name: 'Tyler Suderman',
     title: 'Staff Engineer',
     milestoneByTrack: {
-      'MOBILE': 1,
-      'WEB_CLIENT': 2,
-      'FOUNDATIONS': 3,
-      'SERVERS': 2,
-      'PROJECT_MANAGEMENT': 4,
-      'COMMUNICATION': 1,
-      'CRAFT': 1,
-      'INITIATIVE': 4,
-      'CAREER_DEVELOPMENT': 3,
-      'ORG_DESIGN': 2,
-      'WELLBEING': 0,
-      'ACCOMPLISHMENT': 4,
-      'MENTORSHIP': 2,
-      'EVANGELISM': 2,
-      'RECRUITING': 3,
-      'COMMUNITY': 0
+      'SELF': 3,
+      'TEAM': 1,
+      'PEERS': 2,
+      'SUPERIORS': 1,
+      'BUSINESS': 3,
+      'HEALTH': 2
     },
-    focusedTrackId: 'MOBILE'
+    focusedTrackId: 'SELF'
   }
 }
 
