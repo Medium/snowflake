639c1078d2406c25ca4d8d1bc16006305e0cc43f
Bruno Farache <bruno.farache@liferay.com>
2018-03-27 11:10:36 -0300

    Add back WEB_CLIENT milestone, it was removed by mistake

--- constants.js
+++ constants.js
@@ -177,6 +177,73 @@ export const tracks: Tracks = {
     }]
   },
 
+  "WEB_CLIENT": {
+    "displayName": "Web client",
+    "category": "A",
+    "description": "Develops expertise in web client technologies, such as HTML, CSS, and JavaScript",
+    "milestones": [{
+      "summary": "Works effectively within established web client architectures, following current best practices",
+      "signals": [
+        "Makes minor modifications to existing screens",
+        "Fixes simple design quality issues",
+        "Uses CSS appropriately, following style guide",
+      ],
+      "examples": [
+        "Implemented sticky footer on the post page",
+        "Hooked up the action to dismiss a post from a stream",
+        "Built PaymentHistory screen using ResponseScreen",
+      ],
+    }, {
+      "summary": "Develops new instances of existing architecture, or minor improvements to existing architecture",
+      "signals": [
+        "Makes sensible abstractions based on template and code patterns",
+        "Specs and builds interactive components independently",
+        "Prototypes simple new features quickly",
+      ],
+      "examples": [
+        "Built credit card input component",
+        "Created shared buttons template",
+        "Built modal system",
+      ],
+    }, {
+      "summary": "Designs major new features and demonstrates a nuanced understanding of browser constraints",
+      "signals": [
+        "Provides useful design feedback and suggests feasible alternatives",
+        "Performs systemic tasks to significantly minimise bundle size",
+        "Acts a caretaker for all of web client code",
+      ],
+      "examples": [
+        "Designed font loading strategy for Medium",
+        "Researched utility of service workers for Medium",
+        "Designed and implemented ResponseScreen",
+      ],
+    }, {
+      "summary": "Builds complex, reusable architectures that pioneer best practices and enable engineers to work more effectively",
+      "signals": [
+        "Pioneers architecture migrations that reduce programmer burden",
+        "Implements complex UI transitions that bring delight",
+        "Makes architectural decisions that eliminate entire classes of bugs",
+      ],
+      "examples": [
+        "Designed Medium's post morpher and delta system",
+        "Implemented Medium's scrolling text over image blur",
+        "Designed and pioneered proto-based model storage",
+      ],
+    }, {
+      "summary": "Is an industry-leading expert in web client or sets strategic web client direction for an eng team",
+      "signals": [
+        "Invents new techniques to innovate and overcome browser constraints",
+        "Identifies and solved systemic problems with current architecture",
+        "Defines a long-term vision for web client and ensures projects are in service of it",
+      ],
+      "examples": [
+        "Invented CSS in JS",
+        "Defined and drove migration strategy to Lite",
+        "Implemented unidirectional data flow to completion",
+      ],
+    }],
+  },
+
   "FOUNDATIONS": {
     "displayName": "Foundations",
     "category": "A",
