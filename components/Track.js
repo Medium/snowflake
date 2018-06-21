3038016f92c8aa11d52189c0f7cf32ed5703ba51
Scott Burns <scott.s.burns@gmail.com>
2018-06-21 07:40:15 -0500

    Prettify Track.js

--- components/Track.js
+++ components/Track.js
@@ -1,20 +1,20 @@
 // @flow
 
-import { tracks, milestones, categoryColorScale } from '../constants'
-import React from 'react'
-import type { MilestoneMap, TrackId, Milestone } from '../constants'
+import { tracks, milestones, categoryColorScale } from "../constants";
+import React from "react";
+import type { MilestoneMap, TrackId, Milestone } from "../constants";
 
 type Props = {
   milestoneByTrack: MilestoneMap,
   trackId: TrackId,
   handleTrackMilestoneChangeFn: (TrackId, Milestone) => void
-}
+};
 
 class Track extends React.Component<Props> {
   render() {
-    const track = tracks[this.props.trackId]
-    const currentMilestoneId = this.props.milestoneByTrack[this.props.trackId]
-    const currentMilestone = track.milestones[currentMilestoneId - 1]
+    const track = tracks[this.props.trackId];
+    const currentMilestoneId = this.props.milestoneByTrack[this.props.trackId];
+    const currentMilestone = track.milestones[currentMilestoneId - 1];
     return (
       <div className="track">
         <style jsx>{`
@@ -50,24 +50,45 @@ class Track extends React.Component<Props> {
         `}</style>
         <h2>{track.displayName}</h2>
         <p className="track-description">{track.description}</p>
-        <div style={{display: 'flex'}}>
-          <table style={{flex: 0, marginRight: 50}}>
+        <div style={{ display: "flex" }}>
+          <table style={{ flex: 0, marginRight: 50 }}>
             <tbody>
-              {milestones.slice().reverse().map((milestone) => {
-                const isMet = milestone <= currentMilestoneId
-                return (
-                  <tr key={milestone}>
-                    <td onClick={() => this.props.handleTrackMilestoneChangeFn(this.props.trackId, milestone)}
-                        style={{border: `4px solid ${milestone === currentMilestoneId ? '#000' : isMet ? categoryColorScale(track.category) : '#eee'}`, background: isMet ? categoryColorScale(track.category) : undefined}}>
-                      {milestone}
-                    </td>
-                  </tr>
-                )
-              })}
+              {milestones
+                .slice()
+                .reverse()
+                .map(milestone => {
+                  const isMet = milestone <= currentMilestoneId;
+                  return (
+                    <tr key={milestone}>
+                      <td
+                        onClick={() =>
+                          this.props.handleTrackMilestoneChangeFn(
+                            this.props.trackId,
+                            milestone
+                          )
+                        }
+                        style={{
+                          border: `4px solid ${
+                            milestone === currentMilestoneId
+                              ? "#000"
+                              : isMet
+                                ? categoryColorScale(track.category)
+                                : "#eee"
+                          }`,
+                          background: isMet
+                            ? categoryColorScale(track.category)
+                            : undefined
+                        }}
+                      >
+                        {milestone}
+                      </td>
+                    </tr>
+                  );
+                })}
             </tbody>
           </table>
           {currentMilestone ? (
-            <div style={{flex: 1}}>
+            <div style={{ flex: 1 }}>
               <h3>{currentMilestone.summary}</h3>
               <h4>Example behaviors:</h4>
               <ul>
@@ -85,8 +106,8 @@ class Track extends React.Component<Props> {
           ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Track
+export default Track;
