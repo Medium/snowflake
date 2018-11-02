6d00a66eab3daa90354c61765407f57510259267
Michael Große <michael.grosse@wikimedia.de>
2018-11-02 15:42:16 +0100

    Extract Help icon into new component
    The advantage of this is that we can add the logic for
    showing the help modal into this new component and later
    move it around as-is if we want.

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 // @flow
 
 import React from 'react'
+import Help from "./Help";
 
 type Props = {
   name: String,
   setNameFn: (string) => void
 }
 
-class Header extends React.Component<Props> {
+class Header extends React.PureComponent<Props> {
   render() {
     return (
       <div style={{display: 'flex', 'justify-content': 'space-between', outline: '1px red solid'}}>
@@ -39,11 +40,7 @@ class Header extends React.Component<Props> {
 
         </form>
 
-        <div title="FIXME Add about text here" style={{width:'110px', fill: 'grey'}}>
-          <svg viewBox="0 0 24 24">
-            <path d="M11,18H13V16H11V18M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,6A4,4 0 0,0 8,10H10A2,2 0 0,1 12,8A2,2 0 0,1 14,10C14,12 11,11.75 11,15H13C13,12.75 16,12.5 16,10A4,4 0 0,0 12,6Z" />
-          </svg>
-        </div>
+        <Help />
       </div>
     );
   }
