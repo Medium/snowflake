66de7758d175bdcf71ad0b9f7c380a388cd6f5b2
Matt Bullock <matthew.bullock@prodigygame.com>
2019-03-10 20:11:41 -0400

    Added empty domain template

---
+++ track-definitions/_empty-template.js
@@ -0,0 +1,276 @@
+import commonTracks from './shared';
+
+const tracks = {
+  "1": {
+    "displayName": "",
+    "category": "A",
+    "description": "",
+    "milestones": [{
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }],
+  },
+
+  "2": {
+    "displayName": "",
+    "category": "A",
+    "description": "",
+    "milestones": [{
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }],
+  },
+
+  "3": {
+    "displayName": "",
+    "category": "A",
+    "description": "",
+    "milestones": [{
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }],
+  },
+
+  "4": {
+    "displayName": "",
+    "category": "A",
+    "description": "",
+    "milestones": [{
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }, {
+      "summary": "",
+      "signals": [
+        "",
+        "",
+        "",
+      ],
+      "examples": [
+        "",
+        "",
+        "",
+      ],
+    }],
+  },
+}
+
+export default {
+  ...tracks,
+  ...commonTracks
+}
