afc08b432ab38cffb01bdebe0a5d9e50a7c48ddc
Fhenon De Urioste <fhenon.deurioste@iadvize.com>
2019-08-29 16:10:17 +0200

    test() : fetch with isomorphic

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 import SnowflakeApp from '../components/SnowflakeApp'
+import fetch from 'isomorphic-unfetch'
 
-export default () => <div>
-  <SnowflakeApp />
-</div>
+const Index = props => (
+  <div>
+    {props.shows.map(show => (
+        <li key={show.id}>
+          <a>{show.name}</a>
+        </li>
+      ))}
+    <SnowflakeApp />
+  </div>
+);
+
+Index.getInitialProps = async function(plop) {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+  console.log('plop', Object.keys(plop.query));
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
