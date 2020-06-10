ebf80f97cf0d5fa6c4b70c1226bbab5a3fc91b11
calvin-fisher <1797159+calvin-fisher@users.noreply.github.com>
2020-06-10 12:59:48 -0500

    Fixed export filename date

--- components/FileImportExport.tsx
+++ components/FileImportExport.tsx
@@ -28,8 +28,8 @@ class FileImportExport extends React.Component<Props> {
   getFilename() {
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth().toString().padStart(2, "0");
-    const day = date.getDay().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
     return `${this.props.name} ${year}-${month}-${day}.json`;
   }
 
